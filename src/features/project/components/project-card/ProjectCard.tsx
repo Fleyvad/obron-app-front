d21ca488dbf49760227909fb62737af8eedd8432
Paco Leyva <118618592+Fleyvad@users.noreply.github.com>
2023-03-28 14:02:43 +0200

    create project card and add img for links

---
+++ src/features/project/components/project-card/ProjectCard.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import Project from '../../projects-model';
+import { TextCard } from './project-card-styled';
+
+interface projectCardProps {
+  projects: Project;
+}
+
+const ProjectCard: FC<projectCardProps> = ({ projects }) => {
+  const { projectName, date, imgUrl } = projects;
+  const { enterprise } = projects.resources;
+
+  return (
+    <>
+      <Link className="card__link" to={''}>
+        <div className="card__texts">
+          <TextCard>{projectName}</TextCard>
+          <TextCard>{date}</TextCard>
+          <TextCard>{enterprise}</TextCard>
+        </div>
+        <div className="card__logos">
+          <img
+            src="/assets/img/clock.svg"
+            className="card__priority-logo"
+            alt="clock-priority"
+          />
+
+          <img
+            src="/assets/img/delete.svg"
+            className="card__delete-logo"
+            alt="delete"
+          />
+        </div>
+      </Link>
+      <img src={imgUrl} className="card__img" alt="dsfsd" />
+    </>
+  );
+};
+
+export default ProjectCard;
---
+++ src/features/project/components/project-card/project-card-styled.ts
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+export const ProjectContainer = styled.li`
+  margin: 1rem auto;
+  border: 1px solid black;
+  border-radius: 12px;
+  list-style: none;
+  min-height: 5rem;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  background-color: #ffcf6a;
+  box-shadow: rgb(51, 51, 51) 2px 2px 0px 0px;
+
+  .card__link {
+    text-decoration: none;
+    display: flex;
+    color: black;
+    font-size: 1rem;
+    justify-content: space-between;
+  }
+  .card__img {
+    width: 100%;
+    height: 30%;
+    width: 100%;
+    height: 15rem;
+    border-radius: 0 0 12px 12px;
+    object-fit: cover;
+    object-position: 50% 50%;
+    display: none;
+  }
+  .card__priority-logo {
+    margin-right: 0.5rem;
+  }
+
+  .card__texts {
+    display: flex;
+  }
+  .card__logos {
+    display: flex;
+  }
+  @media (max-width: 800px) {
+    padding: 0;
+    .card__img {
+      display: block;
+      border-top: 1px solid black;
+    }
+    .card__link {
+      padding: 1rem;
+    }
+    .card__texts {
+      display: block;
+      border-color: none;
+    }
+    .card__logos {
+      display: flex;
+      align-items: flex-start;
+    }
+    .card__delete-logo {
+      margin-top: 0.15rem;
+    }
+  }
+`;
+
+export const TextCard = styled.p`
+  padding: 0 2rem;
+  border-right: 1px solid black;
+  @media (max-width: 800px) {
+    margin-bottom: 1rem;
+    border-bottom: 1px solid black;
+    border-right: none;
+    padding: 0;
+  }
+`;
