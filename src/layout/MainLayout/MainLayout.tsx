2ca91db23d04cf1fc5083b5d9f71259adc3716b3
Paco Leyva <118618592+Fleyvad@users.noreply.github.com>
2023-03-29 13:18:34 +0200

    create header componente and refactor main layout

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../../shared/components/Header';
 
 const MainLayout = () => {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
