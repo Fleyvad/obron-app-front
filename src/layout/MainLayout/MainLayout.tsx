90e7fb841c9bce5f1996b9e7da067eb8f762d4ba
Paco Leyva <118618592+Fleyvad@users.noreply.github.com>
2023-03-29 19:41:07 +0200

    modify route header

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Header from '../../shared/components/Header';
+import Header from '../../shared/components/Header/Header';
 
 const MainLayout = () => {
   return (
