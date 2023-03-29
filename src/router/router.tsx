3214d9d5858e855ed557f5863e7e55b91139d0bd
Paco Leyva <118618592+Fleyvad@users.noreply.github.com>
2023-03-29 19:47:24 +0200

    modify style login page and add route not found page to router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../layout/MainLayout/MainLayout';
 import Login from '../pages/Login/Login';
+import { NotFoundPage } from '../pages/not-found/NotFound';
 import ProjectsPage from '../pages/projects/Projects';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Login />,
+    errorElement: <NotFoundPage />,
   },
   {
     path: '/',
